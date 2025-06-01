eacffd798bf4245cb8be0c32e04ba370a1a70aa0
Andrei Ungureanu <andreiungureanu2014@yahoo.ro>
2025-06-02 00:10:34 +0300

    restored initial query

--- SocialApp/ServerLibraryProject/Repositories/PostRepository.cs
+++ SocialApp/ServerLibraryProject/Repositories/PostRepository.cs
@@ -78,18 +78,13 @@ namespace ServerLibraryProject.Repositories
 
         public List<Post> GetPostsHomeFeed(long userId)
         {
-            //var postsQuery = from post in dbContext.Posts
-            //                 where post.UserId == userId
-            //                    || post.Visibility == PostVisibility.Public
-            //                    || dbContext.UserFollowers.Any(userFollower =>
-            //                           (userFollower.FollowerId == userId
-            //                        || userFollower.UserId == post.UserId)
-            //                        && (post.Visibility == PostVisibility.Friends))
-            //                 orderby post.CreatedDate descending
-            //                 select post;
             var postsQuery = from post in dbContext.Posts
                              where post.UserId == userId
                                 || post.Visibility == PostVisibility.Public
+                                || dbContext.UserFollowers.Any(userFollower =>
+                                       (userFollower.FollowerId == userId
+                                    || userFollower.UserId == post.UserId)
+                                    && (post.Visibility == PostVisibility.Friends))
                              orderby post.CreatedDate descending
                              select post;
 
